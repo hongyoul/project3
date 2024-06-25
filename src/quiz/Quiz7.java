fbdedb5f742977d6c01be984aa981e5b04762a23
Hongyoul <hongyoul03e@gmail.com>
2024-06-25 16:56:45 +0900

    2024.06.25

--- src/quiz/Quiz7.java
+++ src/quiz/Quiz7.java
@@ -6,7 +6,9 @@ public class Quiz7 {
 		
 		
 		//코드를 보고 연산과정과 결과값을 주석으로 작성하세요.
-		boolean resul1 = (1 == 2) || (1 < 2); // true , 연산자 총 4개 사용됨 *우선순위: 비교연산자>논리연산자
+		boolean resul1 = (1 == 2) || (1 < 2); // true , 연산자 총 4개 사용됨
+		//*우선순위: 비교연산자>논리연산자
+		
 		boolean resul2 = true && false; // false
 		boolean resul3 = !false; // true
 		// *논리연산과 비교연산의 식은 여러개(2개이상) 사용이 가능하고
